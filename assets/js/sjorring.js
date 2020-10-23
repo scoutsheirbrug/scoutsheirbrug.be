0ca31ca307b1fc078b964ee541805fac08cab9f3
mstuyven <mirostuyven@gmail.com>
2020-10-23 15:08:12 +0200

    Add external link and fallback to pdf viewer

--- assets/js/sjorring.js
+++ assets/js/sjorring.js
@@ -1,16 +1,23 @@
 let currentPage = 0
 let pages = [];
 
-const canvas = document.querySelector('canvas[data-pdf]');
-const loadingTask = pdfjsLib.getDocument(canvas.getAttribute('data-pdf'));
+const viewer = document.querySelector('.pdf-viewer[data-pdf]');
 
 /* Read document */
 (async () => {
-  const pdf = await loadingTask.promise;
-  pages = await Promise.all([...Array(pdf.numPages).keys()].map(i => pdf.getPage(i+1)));
-  updatePage()
+  try {
+    const loadingTask = pdfjsLib.getDocument(viewer.getAttribute('data-pdf'));
+    const pdf = await loadingTask.promise;
+    pages = await Promise.all([...Array(pdf.numPages).keys()].map(i => pdf.getPage(i+1)));
+    updatePage()
+  } catch (e) {
+    pages = []
+    viewer.classList.add('failed')
+    viewer.querySelector('embed').setAttribute('src', viewer.getAttribute('data-pdf'))
+  }
 })();
 
+/* Page controls */
 document.querySelectorAll('[data-page]').forEach(el => {
   el.addEventListener('click', () => {
     const targetPage = currentPage + parseInt(el.getAttribute('data-page'))
@@ -24,6 +31,7 @@ document.querySelectorAll('[data-page]').forEach(el => {
 async function updatePage() {
   const page = pages[currentPage];
   const viewport = page.getViewport({ scale: 2 });
+  const canvas = viewer.querySelector('canvas');
   const canvasContext = canvas.getContext("2d");
   canvas.height = viewport.height;
   canvas.width = viewport.width;
@@ -32,4 +40,8 @@ async function updatePage() {
 
 document.querySelector('.pdf-enlarge').addEventListener('click', () => {
   document.body.classList.toggle('pdf-enlarged')
+})
+
+document.querySelector('.pdf-external').addEventListener('click', () => {
+  location.href = viewer.getAttribute('data-pdf')
 })
