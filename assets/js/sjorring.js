7d5118c6789346d7d316140fb6c5a87b47c6a6f2
mstuyven <mirostuyven@gmail.com>
2021-03-24 23:38:55 +0100

    Improve sjorring viewer - render 2 pages if possible - prerender all pages after loading

--- assets/js/sjorring.js
+++ assets/js/sjorring.js
@@ -1,5 +1,7 @@
-let currentPage = 0
+let currentPage = 0;
 let pages = [];
+let renderedPages = [];
+let renderTasks = [];
 
 const viewer = document.querySelector('.pdf-viewer[data-pdf]');
 
@@ -9,7 +11,8 @@ const viewer = document.querySelector('.pdf-viewer[data-pdf]');
     const loadingTask = pdfjsLib.getDocument(viewer.getAttribute('data-pdf'));
     const pdf = await loadingTask.promise;
     pages = await Promise.all([...Array(pdf.numPages).keys()].map(i => pdf.getPage(i+1)));
-    updatePage()
+    updatePages();
+    renderAllPages();
   } catch (e) {
     pages = []
     viewer.classList.add('failed')
@@ -20,22 +23,58 @@ const viewer = document.querySelector('.pdf-viewer[data-pdf]');
 /* Page controls */
 document.querySelectorAll('[data-page]').forEach(el => {
   el.addEventListener('click', () => {
-    const targetPage = currentPage + parseInt(el.getAttribute('data-page'))
-    if (targetPage >= 0 && targetPage < pages.length) {
+    let delta = parseInt(el.getAttribute('data-page'));
+    visiblePages = numVisiblePages();
+    delta *= visiblePages;
+    const targetPage = Math.max(0, Math.min(pages.length - visiblePages, currentPage + delta));
+    if (targetPage !== currentPage) {
       currentPage = targetPage;
-      updatePage()
+      updatePages();
     }
   })
 })
 
-async function updatePage() {
-  const page = pages[currentPage];
-  const viewport = page.getViewport({ scale: 2 });
-  const canvas = viewer.querySelector('canvas');
+function updatePages() {
+  renderPage(viewer.querySelector('.page-left'), currentPage, 0);
+  renderPage(viewer.querySelector('.page-right'), currentPage + 1, 1);
+}
+
+function numVisiblePages() {
+  if (document.body.classList.contains('pdf-enlarged')) {
+    return 1;
+  }
+  return window.matchMedia('(max-aspect-ratio: 8/5)').matches ? 1 : 2;
+}
+
+async function renderPage(canvas, index, task) {
   const canvasContext = canvas.getContext("2d");
+  if (renderedPages[index]) {
+    canvasContext.putImageData(renderedPages[index], 0, 0);
+    return;
+  }
+  const page = pages[index];
+  if (!page) {
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    return;
+  }
+  const viewport = page.getViewport({ scale: 2 });
   canvas.height = viewport.height;
   canvas.width = viewport.width;
-  await page.render({ canvasContext, viewport });
+  if (renderTasks[task]) {
+    renderTasks[task].cancel();
+  }
+  const renderTask = page.render({ canvasContext, viewport });
+  renderTasks[task] = renderTask;
+  return renderTask.promise.then(() => {
+    renderedPages[index] = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+  });
+}
+
+async function renderAllPages() {
+  for (let i = 0; i < pages.length; i += 1) {
+    await renderPage(viewer.querySelector('.page-hidden'), i, 2);
+  }
 }
 
 document.querySelector('.pdf-enlarge').addEventListener('click', () => {
