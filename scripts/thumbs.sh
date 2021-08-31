46db193f754a05153682e45c560b137acc27fd5f
mstuyven <mirostuyven@gmail.com>
2021-08-31 19:02:02 +0200

    Kleinkamp 2021 fotos

--- scripts/thumbs.sh
+++ scripts/thumbs.sh
@@ -1,3 +1,10 @@
-for i in assets/fotos/*; do mkdir -p "${i/fotos/thumbs\/fotos}"; done
+#!/usr/bin/env bash
 
-for i in assets/fotos/*/*.jpg; do ffmpeg -y -loglevel error -i "$i" -vf "crop=w='min(iw\,ih)':h='min(iw\,ih)',scale=256:256,setsar=1" "${i/fotos/thumbs\/fotos}"; echo "Created thumb for $i"; done
+for f in assets/fotos/*/; do
+    mkdir -p "${f/fotos/thumbs\/fotos}"
+    magick mogrify -path "${f/fotos/thumbs\/fotos}" \
+        -resize 256x256^ \
+        -gravity Center \
+        -extent 256x256 \
+        "$f*.jpg"
+done
